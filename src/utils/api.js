7f1a4fab9735dccaeb685dadd09b654a316b69e4
Aryan6400 <aryan6400cool@gmail.com>
2024-03-30 00:16:41 +0530

    Added create page and functionality

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,12 @@
-import { post } from './axios'
+import { get, post } from './axios'
 
 export const login = async(url, data) => post({url,data})
 export const register = async(url, data) => post({url,data})
 export const sendOTP = async(url, data) => post({url,data})
 export const verifyOTP = async(url, data) => post({url,data})
 export const googleLogin = async(url, data) => post({url,data})
+
+export const searchTags = async(url, data) => get({url,data})
+export const createDraft = async(url, data) => post({url,data})
+export const getDraftDetails = async(url) => get({url})
+export const createBlog = async(url, data) => post({url,data})
