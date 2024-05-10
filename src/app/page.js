b54c8a514b568834c5d84c05c094b94f9b55ae98
Aryan6400 <aryan6400cool@gmail.com>
2024-05-10 23:35:28 +0530

    adding prettier in pre-commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,32 @@
-import classes from './page.module.css'
-import InfoLink from "@components/infolink/InfoLink";
-import Attractions from "@components/attractions/Attractions";
-import Hero from "@components/hero/Hero";
-import Itineary from "@components/itineary/Itineary";
-import FeedContainer from "@containers/Feed/FeedContainer";
+import classes from './page.module.css';
+import InfoLink from '@components/infolink/InfoLink';
+import Attractions from '@components/attractions/Attractions';
+import Hero from '@components/hero/Hero';
+import Itineary from '@components/itineary/Itineary';
+import FeedContainer from '@containers/Feed/FeedContainer';
 
 const getData = async () => {
-  const base_url = process.env.NEXT_PUBLIC_BACKEND_URL
+  const base_url = process.env.NEXT_PUBLIC_BACKEND_URL;
   const response = await fetch(`${base_url}/blog/all?limit=10&skip=0`, {
-    cache: 'no-store'
-  })
+    cache: 'no-store',
+  });
   if (response.status === 200) {
-    const res = await response.json()
-    return res
+    const res = await response.json();
+    return res;
+  } else {
+    return [];
   }
-  else {
-    return []
-  }
-}
+};
 
 export default async function Home() {
-  const data = await getData()
+  const data = await getData();
   return (
     <>
       <Hero />
       <Attractions top={true} />
       <div className={classes.homepage}>
         <FeedContainer initialData={data} />
-        <div className={classes["homepage-right"]}>
+        <div className={classes['homepage-right']}>
           <InfoLink />
           <Attractions />
           <Itineary />
