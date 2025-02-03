f5ba0a19f164da93aa6399eb6a9c14b719336eda
Aryan6400 <aryan6400cool@gmail.com>
2025-02-03 14:19:18 +0530

    Added sentry monitoring

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,19 @@ import Itineary from '@components/itineary/Itineary';
 import FeedContainer from '@containers/Feed/FeedContainer';
 
 const getData = async () => {
-  const base_url = process.env.NEXT_PUBLIC_BACKEND_URL;
-  const response = await fetch(`${base_url}/blog/all?limit=10&skip=0`, {
-    cache: 'no-store',
-  });
-  if (response.status === 200) {
-    const res = await response.json();
-    return res;
-  } else {
+  try {
+    const base_url = process.env.NEXT_PUBLIC_BACKEND_URL;
+    const response = await fetch(`${base_url}/blog/all?limit=10&skip=0`, {
+      cache: 'no-store',
+    });
+    if (response.status === 200) {
+      const res = await response.json();
+      return res;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    console.error(error.message);
     return [];
   }
 };
